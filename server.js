ba0a6f2de45ba5ca524578fe3ea792c57e2d0f94
dervasavred <97542941+dervasavred@users.noreply.github.com>
2022-06-14 19:23:39 -0400

    Add MongoDB connection and add basic GET function

--- server.js
+++ server.js
@@ -1,24 +1,72 @@
-const res = require("express/lib/response");
-
 express = require("express");
 app = express();
 
-const MongoClient = require("mongodb").MongoClient;
+// ENVIRONMENT VARS
+  require("dotenv").config();
 
-PORT = 54000;
+// DB CONNECTION
+  const MongoClient = require("mongodb").MongoClient;
+
+  let db,
+      dbConnectionStr = process.env.DB_STRING,
+      dbName = "hoursTracker";
+
+  MongoClient.connect(dbConnectionStr)
+      .then( client => {
+        console.log(`Connected to ${dbName} Database`);
+        db = client.db(dbName);
+      }); // END THEN
+
+
+// LOCAL PORT TO RUN ON
+  PORT = 54000;
+
+// CORS HANDLER
+  const cors = require("cors");
+  app.use( cors() );
+
+// SET VIEW ENGINE
+  app.set("view engine", "ejs");
+
+// SERVE STATIC FILES AT WILL
+  app.use(express.static("public"));
+
+// ACCEPT URL ENCODING METHODS?
+  app.use(express.urlencoded({extended: true}));
+
+// ENABLES EXPRESS TO PARSE JSON INSTEAD OF NEEDING BODYPARSER MODULE
+  app.use(express.json());
 
 // ==================================================
-//          SERVER HANDLERS
+//          SERVER HANDLERS (CRUD)
 // ==================================================
 
-app.get("/", ( req, res) ) {
-  res.sendFile(ROOT_DIR + "index.html");
-}; // END GET
+app.get("/", (req, res)  => {
+  const contents = db.collection("locations").find().toArray()
+    .then(data => {
+      let nameList = data.map(ele => ele.name);
+      console.log(nameList);
+      res.render("index.ejs", {info: nameList});
+    });
 
-app.get("/api", ( req, res) ) {
+}); // END GET
 
-}; // END GET
+app.post("/api", (req, res)  => {
 
-app.get("/", ( req, res) ) {
+}); // END GET
 
-}; // END GET
+app.put("updateEntry", (req, res)  => {
+
+}); // END GET
+
+app.delete("/removeEntry", (req, res)  => {
+
+}); // END GET
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+}); // END LISTEN
+
+// app.get("/", ( req, res) ) {
+
+// }; // END GET
